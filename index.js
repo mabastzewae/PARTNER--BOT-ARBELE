237cad58adcdad0e0591283cb354c1da4b67a7a1
mabastzewae <76887190+mabastzewae@users.noreply.github.com>
2021-01-07 13:41:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -36,9 +36,9 @@ if(message.channel.type === 'dm') {
 
 if (message.content.startsWith("https://discord.gg/")) {
     
-  message.author.send(``) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
+  message.author.send(`https://discord.gg/XrrZzFrxHm`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
     
-  client.channels.get("id جەنالەکە").send(
+  client.channels.get("788809021929816122").send(
 `> sent By  <@${message.author.id}> ✅ 
 ${message.content}`)
 }
@@ -51,10 +51,10 @@ ${message.content}`)
 client.on('ready', () => {
 console.log(`ON ${client.guilds.size} Servers`);
 console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`شتێک بنوسە بۆ ستریمینگ`,"http://twitch.tv/PAYWANDcamper") 
+client.user.setGame(`ریکلام بۆ من بنێرە`,"http://twitch.tv/PAYWANDcamper") 
 });
 
 
 
 
-client.login("");//تۆکین لێرە دانێ
+client.login("Nzc3NjM4MzA4MDQ1MjU4NzY0.X7GWHA.sKmPp4WMimb9tV6mubAoxZ1b71Q");//تۆکین لێرە دانێ
